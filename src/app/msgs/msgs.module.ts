1347737788d00cbd270d24252b83355480729e4b
mbeckius@gmail.com <MBeckius@gmail.com>
2018-02-06 17:01:08 -0500

    sidebar

--- src/app/msgs/msgs.module.ts
+++ src/app/msgs/msgs.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Route, Router, RouterModule } from '@angular/router';
+import { MsgListComponent } from './msg-list/msg-list.component';
+
+const routes: Route[] = [
+  {
+    path: 'msgs',
+    outlet: 'sidebar',
+    component: MsgListComponent
+   }
+];
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routes)
   ],
-  declarations: []
+  declarations: [MsgListComponent]
 })
 export class MsgsModule { }
